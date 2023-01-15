370a93b97de33b0f73687c88c3b5f20b26dd2e13
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-01-15 16:07:41 +0300

    search jobs

--- axios/axios.ts
+++ axios/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
-  // baseURL: "https://tuttor.vercel.app/api/",
+  // baseURL: "http://localhost:3000/api/",
+  baseURL: "https://tuttor.vercel.app/api/",
   withCredentials: true,
 })
 
